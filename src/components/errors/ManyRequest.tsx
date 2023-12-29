6d4ee1a9662e99df46037822f4d196ea272c8035
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-12-29 17:00:04 +0700

    add error page

---
+++ src/components/errors/ManyRequest.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ManyRequest = () => {
+  return (
+    <section className="flex min-h-screen justify-center items-center">
+      <h1 className="text-xl text-slate-700">
+        Kamu terlalu banyak request sayang...
+      </h1>
+    </section>
+  );
+};
+
+export default ManyRequest;
