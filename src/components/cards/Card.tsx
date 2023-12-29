0b03cd140747ce146d983c9732cf5198b2d551e2
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-12-29 16:59:58 +0700

    add card component

---
+++ src/components/cards/Card.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface CardProps {
+  item: any;
+}
+
+const Card: React.FC<CardProps> = ({ item }) => {
+  return (
+    <div className="relative">
+      <div className="rounded overflow-hidden">
+        <img
+          src={item?.images?.jpg?.large_image_url}
+          alt="Anime Thumbnail"
+          className="w-full object-contain hover:scale-125 transition-all ease duration-300"
+        />
+      </div>
+      <div className="w-full p-2 absolute left-0 bottom-0 bg-gradient-to-b from-transparent to-black">
+        <p className="text-primary-dark">{item?.title_japanese}</p>
+        <h3 className="text-lg font-semibold text-primary-dark hover:underline">
+          <a href={item?.url} target="_blank">
+            {item?.title}
+          </a>
+        </h3>
+        <p className="text-primary-dark flex justify-between">
+          <small>{item?.episodes} Episode</small>
+          <small className="text-yellow-400">{item?.score} 🌟</small>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
