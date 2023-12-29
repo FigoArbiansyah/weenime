9feca91b0cb58a6690ca02f02c3a4fd72d47671e
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-12-29 17:21:00 +0700

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-6 bg-slate-100 text-slate-800">
+      <p>
+        Created with ❤️ by:{" "}
+        <a href="https://www.github.com/figo_arbnsyh" target="_blank">
+          Figo Arbiansyah
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
