51c409c892e44e12395b37a6b857dbadd2b47f77
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-12-29 17:23:19 +0700

    update Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,23 @@
 import { useState, useEffect } from "react";
 import ListAnimeLoading from "../components/loadings/ListAnimeLoading";
+import ManyRequest from "../components/errors/ManyRequest";
+import Card from "../components/cards/Card";
+import Button from "../components/Button";
+import {
+  getItem as getStorageItem,
+  setItem as setStorageItem,
+} from "../helpers/localStorage";
+import Footer from "../components/Footer";
 
 const { VITE_BASE_URL } = import.meta.env;
+const CURRENT_PAGE = getStorageItem("page") || 1;
 
 const Home = () => {
   const [animeList, setAnimeList] = useState<any[]>([]);
   const [metadata, setMetadata] = useState<any>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<unknown>();
-  const [page, setPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(CURRENT_PAGE);
 
   const fetchData = async () => {
     setLoading(true);
@@ -32,82 +41,57 @@ const Home = () => {
     fetchData();
   }, [page]);
 
-  animeList && console.log(animeList);
-
   if (error) {
-    return (
-      <section className="flex min-h-screen justify-center items-center">
-        <h1 className="text-xl text-slate-700">
-          Kamu terlalu banyak request sayang...
-        </h1>
-      </section>
-    );
+    return <ManyRequest />; // Error page
   }
 
   return (
     <div className="bg-primary-dark">
-      <main className="min-h-screen md:p-10 p-5 max-md:py-10">
+      <main className="min-h-screen md:p-10 max-md:py-10 max-md:px-4">
         <section className="pb-10">
           <div className="text-center">
-            <h1 className="text-3xl font-bold text-primary-light">Weenime</h1>
+            <h1 className="text-3xl font-bold bg-clip-text bg-gradient-to-r from-emerald-400 to-indigo-500 text-transparent">
+              Weenime
+            </h1>
           </div>
         </section>
         {loading ? (
           <ListAnimeLoading />
         ) : (
-          <section className="grid md:grid-cols-5 grid-cols-1 gap-4">
+          <section className="grid md:grid-cols-5 grid-cols-2 gap-4">
             {animeList?.map((anime, index) => {
-              return (
-                <div key={index} className="relative">
-                  <div className="rounded overflow-hidden">
-                    <img
-                      src={anime?.images?.jpg?.large_image_url}
-                      alt="Anime Thumbnail"
-                      className="w-full object-contain hover:scale-125 transition-all ease duration-300"
-                    />
-                  </div>
-                  <div className="w-full p-2 absolute left-0 bottom-0 bg-gradient-to-b from-transparent to-black">
-                    <p className="text-primary-dark">{anime?.title_japanese}</p>
-                    <h3 className="text-lg font-semibold text-primary-dark hover:underline">
-                      <a href={anime?.url} target="_blank">
-                        {anime?.title}
-                      </a>
-                    </h3>
-                    <p className="text-primary-dark flex justify-between">
-                      <small>{anime?.episodes} Episode</small>
-                      <small className="text-yellow-400">
-                        {anime?.score} 🌟
-                      </small>
-                    </p>
-                  </div>
-                </div>
-              );
+              return <Card key={index} item={anime} />;
             })}
           </section>
         )}
         <section className="pt-6 flex justify-center items-center">
           <div className="mx-auto flex gap-1">
             {page != 1 && (
-              <button
-                onClick={() => setPage((prev) => prev - 1)}
-                // to={`?page=${page - 1}`}
-                className="inline-block py-2 px-5 rounded bg-rose-500 text-white hover:text-rose-500 border border-rose-500 hover:bg-transparent transition-all ease duration-300"
+              <Button
+                onClick={() => {
+                  const prevPage = page - 1;
+                  setPage(prevPage);
+                  setStorageItem("page", prevPage);
+                }}
               >
                 Prev
-              </button>
+              </Button>
             )}
             {page != metadata?.last_visible_page && (
-              <button
-                onClick={() => setPage((prev) => prev + 1)}
-                // to={`?page=${page + 1}`}
-                className="inline-block py-2 px-5 rounded bg-rose-500 text-white hover:text-rose-500 border border-rose-500 hover:bg-transparent transition-all ease duration-300"
+              <Button
+                onClick={() => {
+                  const nextPage = page + 1;
+                  setPage(nextPage);
+                  setStorageItem("page", nextPage);
+                }}
               >
                 Next
-              </button>
+              </Button>
             )}
           </div>
         </section>
       </main>
+      <Footer />
     </div>
   );
 };
