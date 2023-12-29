a33b1ea809185187ff1222f8fe7ea4bd9df83441
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-12-29 17:28:08 +0700

    update instagram url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="text-center py-6 bg-slate-100 text-slate-800">
       <p>
         Created with ❤️ by:{" "}
-        <a href="https://www.github.com/figo_arbnsyh" target="_blank">
+        <a href="https://www.instagram.com/figo_arbnsyh" target="_blank">
           Figo Arbiansyah
         </a>
       </p>
